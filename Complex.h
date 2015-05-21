7a3c61cc1486360a0483f6372e7938a01b55caed
Simonerror <Drippop@icloud.com>
2015-05-22 02:35:47 +0600

    Create Complex.h

---
+++ Complex.h
@@ -0,0 +1,29 @@
+#include <iostream>
+class Complex
+{	
+	public:
+		Complex();
+		Complex(float Im, float Re);
+
+		Complex operator +(Complex p1);
+		Complex operator -(Complex p1);
+		Complex operator /(Complex p1);
+		Complex operator *(Complex p1);
+
+		Complex conj();
+
+		float abs();
+		float Real();
+		float Imagine();
+
+		friend std::ostream& operator << (std::ostream&c, Complex p);
+		friend std::istream& operator >> (std::istream&c, Complex &p);
+		
+		friend std::ofstream& operator<<(std::ofstream& out, Complex&x);
+		friend std::ifstream& operator>>(std::ifstream& in, Complex&x);
+
+	private:
+		float Im;
+		float Re;
+
+};
